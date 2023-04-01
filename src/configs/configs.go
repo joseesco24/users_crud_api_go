57915e5c979e7991a2f949210864b58cdeccf3fb
“joseesco24” <josed.escobar@outlook.com>
2023-04-01 12:43:29 -0500

    feat: project updates

--- src/configs/configs.go
+++ src/configs/configs.go
@@ -4,8 +4,8 @@ var Config *config = &config{}
 
 type config struct {
 	ApiConfigs struct {
-		AppMode string `env:"APP_MODE" envDefault:"development" validate:"alpha"`
-		AppPort uint16 `env:"APP_PORT" envDefault:"3064" validate:"numeric"`
+		AppMode string `env:"APP_ENVIRONMENT_MODE" validate:"alpha"`
+		AppPort uint16 `env:"APP_SERVER_PORT" validate:"numeric"`
 	}
 }
 
